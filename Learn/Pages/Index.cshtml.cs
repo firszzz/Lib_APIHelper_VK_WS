a6879aecb3487c52013479d35f0b1a81a6405af9
Andrey <n1an27f@gmail.com>
2022-07-27 14:10:45 +1000

    all stat + best post

--- Learn/Pages/Index.cshtml.cs
+++ Learn/Pages/Index.cshtml.cs
@@ -11,10 +11,19 @@ namespace Learn.Pages
         HttpClient client = new HttpClient();
         private readonly ILogger<IndexModel> _logger;
         public string img_src = "https://icons.getbootstrap.com/assets/img/icons-hero.png";
-        public string post_text = "Это текст поста, а значит его нужно добавить где-то сверху, чтобы было красиво.";
+        public string post_text;
         public string likes = "1";
         public string comments = "2";
         public string reposts = "10";
+        public string name;
+        public string screen_name;
+        /*public string description;*/
+        public string[] desc_arr;
+        public string post_count;
+        public string post_likes;
+        public string members;
+        public string best_post_dt;
+        public string best_post_img_src;
 
         APIHelper apiHelper = new APIHelper(token);
 
@@ -27,7 +36,39 @@ namespace Learn.Pages
         {
             Group_Item[] stats_group = await apiHelper.get_stats(client);
             img_src = stats_group[0].photo_200.ToString();
+            name = stats_group[0].name.ToString();
+            /*description = stats_group[0].description.ToString();*/
+            /*desc_arr = description.Split("\n");
+            desc_arr = desc_arr.Where(x => x != "").ToArray();*/
+            screen_name = "https://vk.com/" + stats_group[0].screen_name.ToString();
+            post_count = await apiHelper.get_posts_json(client);
+            JObject json = JObject.Parse(post_count);
+            post_count = json["response"]["count"].ToString();
+            members = stats_group[0].members_count.ToString();
+            post_likes = (await apiHelper.get_likes_sum(client)).ToString();
+            int[] bp = await apiHelper.get_top_reposted(client);
+            string best_post_id = bp[0].ToString();
+            Post_Item[] best_post = await apiHelper.get_post(client, best_post_id);
+            post_text = best_post[0].text;
+            int int_best_post_dt = best_post[0].date;
+            best_post_dt = UnixTimeStampToDateTime(float.Parse(int_best_post_dt.ToString())).ToString();
+            foreach (Attachment item in best_post[0].attachments)
+            {
+                if (item.type == "photo")
+                {
+                    best_post_img_src = item.photo.sizes[2].url;
+                }
+            }
+
             return Page();
+        }
+
+        public static DateTime UnixTimeStampToDateTime(double unixTimeStamp)
+        {
+            // Unix timestamp is seconds past epoch
+            System.DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+            dtDateTime = dtDateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+            return dtDateTime;
         }
     }
 }
